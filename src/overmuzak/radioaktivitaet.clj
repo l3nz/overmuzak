fbf0ae15c6adadf2c0a374f5712d71860d4fa79d
lenz <lenz@mac>
2020-11-04 21:44:54 +0100

    Now with two instruments

--- src/overmuzak/radioaktivitaet.clj
+++ src/overmuzak/radioaktivitaet.clj
@@ -28,24 +28,49 @@
                  :release 0.1
                  :sustain 0)))
 
+(defmethod live/play-note :violin [{midi :pitch seconds :duration amp :amp}]
+  (when midi
+    (println "Playing note" (pitch/find-note-name midi)
+             " for " seconds
+             " (MIDI: " midi ")")
+    (I/violin :midinote midi
+              :dur seconds
+              :amp amp)))
+
 (defn phrase-rakt
   "Radioaktivity riff."
-  [sc]
-  (->> (phrase [1/4 1/2 1/4 1/4 1/4 1/2 1/4 1/2 1/4 1/2]
-               [0 3 3 0 3 0 3 5 4 3])
-       (where :pitch (comp sc scale/major))
-       (all :amp 0.5)))
+  [offset]
+  (->> (phrase [1/2 1 1/2 1/2 1/2 1 1/2 1 1/2 1 2]
+               [0 3 3 0 3 0 3 5 4 3 nil])
+       (where :pitch (fn [p] (+ p offset)))
+       (where :pitch (comp scale/C scale/major))
+       (all :amp 0.5)
+       (all :part :piano)))
+
+(defn pad
+  "Il pad, tipo coro umano"
+
+  [scale mode]
+  (->> (phrase [4]
+               [0])
+       (where :pitch (comp scale mode))
+       (all :amp 0.5)
+       (all :part :violin)))
+
 
 ; trasforma il pitch da nota a hz
+
+
 (def rakt-track
   (->>
    (->> []
-        (then (phrase-rakt scale/A))
-        (then (phrase-rakt scale/F))
-        (then (phrase-rakt scale/G)))
+        (then (->> (phrase-rakt 2)
+                   (with (pad scale/A scale/major)))); A
+        (then (->> (phrase-rakt 0)
+                   (with (pad scale/F scale/major)))) ; F
+        (then (phrase-rakt 1))) ;G
 
-   (all :part :piano)
-   (tempo (bpm 40))))
+   (tempo (bpm 80))))
 
 (comment
   (live/play rakt-track)
